9b7f726b69b8d7b52c2afb9701c89b1ba580964c
Lee-jeong-wook <126264218+Lee-jeong-wook@users.noreply.github.com>
2023-12-28 20:50:56 +0900

    엄청 꾸민 코드~

--- src/pages/N10312.tsx
+++ src/pages/N10312.tsx
@@ -1,9 +1,28 @@
 import { Stack, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 const N10312 = () => {
+  const [value, setValue] = useState(1);
+  const [left, setLeft] = useState(1);
+  const [right, setRight] = useState(1);
+  useEffect(() => {
+    setInterval(() => {
+      const tmp = Math.floor(Math.random() * 300 + 1);
+      const tmp1 = Math.floor(Math.random() * 100 + 1);
+      const tmp2 = Math.floor(Math.random() * 250 + 1);
+      setValue(tmp);
+      setLeft(tmp1);
+      setRight(tmp2);
+    }, 100);
+  }, []);
   return (
     <Stack>
-      <Typography variant='h1'>이정욱</Typography>
+      <Typography
+        variant='h1'
+        style={{ fontSize: `${value}px`, marginLeft: `${value}px`, marginTop: `${value}px` }}
+      >
+        이정욱
+      </Typography>
     </Stack>
   );
 };
